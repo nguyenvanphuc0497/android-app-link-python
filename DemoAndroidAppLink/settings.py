385fe6a7a179872ad941748785315de4176b10cc
Nguyen Van Phuc <nguyenvanphuc0497@gmail.com>
2020-02-26 09:08:50 +0700

    config file static for deploy heroku

--- DemoAndroidAppLink/settings.py
+++ DemoAndroidAppLink/settings.py
@@ -121,3 +121,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # django_heroku.settings(locals())
+
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
